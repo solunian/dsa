c75116bd679a7636d417b4248721d281aaff7f01
Daniel Cui <solunian@gmail.com>
2026-01-28 15:36:20 -0800

    change sort naming

--- tests/inversions_mergesort_test.py
+++ tests/inversions_mergesort_test.py
@@ -1,8 +1,8 @@
 from a.count_inversions import count_inversions as ci
-from a.sort import mergesort
+from a.sort import merge_sort
 
 print(ci([2, 1, 3, 1, 2]))
 print(ci([1, 5, 4, 8, 10, 2, 6, 9, 12, 11, 3, 7]))
 
 
-print(mergesort([1, 5, 4, 8, 10, 2, 6, 9, 12, 11, 3, 7]))
+print(merge_sort([1, 5, 4, 8, 10, 2, 6, 9, 12, 11, 3, 7]))
